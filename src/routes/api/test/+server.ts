4999b61659d1e310b6a8acd1796002a07dd47e6e
Andy Chan <andy@12beesinatrenchco.at>
2026-09-10 02:42:21 -0400

    move logic back into top-plays??

--- src/routes/api/test/+server.ts
+++
@@ -1,9 +0,0 @@
-import { json, type RequestHandler } from "@sveltejs/kit";
-
-export const GET: RequestHandler = async ({ fetch, setHeaders }) => {
-	setHeaders({
-		"cache-control": "public, max-age=20000"
-	});
-
-	return json({ now: Date.now() });
-};
--- src/routes/top-plays/+page.server.ts
+++ src/routes/top-plays/+page.server.ts
@@ -1,22 +1,78 @@
-import { json } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 import type { LyneAPI } from "$lib/types/api";
 import { dev } from "$app/environment";
 
-export const load: PageServerLoad = async ({ fetch, setHeaders }) => {
-	const data = await fetch("/api/top-plays").then(
-		async (response) =>
-			response.json() as unknown as {
-				topPlays: LyneAPI.Score[];
-				date: Date;
-			}
-	);
+const leaderboardLength = 50;
 
-	data.date = new Date(data.date);
+export const load: PageServerLoad = async ({ fetch, setHeaders }) => {
+	// Be nice to ZZZ's servers, use testing
+	// if (dev) {
+	// 	const topPlays = await import("$lib/testing/topPlays.json", { with: { type: "json" } });
+	// 	console.warn("/top-plays: ⚠ RUNNING IN DEV MODE, DATA IS NOT LIVE");
+	// 	return { topPlays: topPlays.default, date: new Date() };
+	// }
+
+	const players: Array<{ username: string; totalPp: number }> = [];
+	let plays: LyneAPI.Score[] = [];
+
+	let lowest = -2;
+
+	// Fill players
+	await fetch("https://api.zxz.moe/rankings/global?scope=all&page=1&pageSize=50")
+		.then(async (response) => {
+			if (!response.ok) {
+				throw new Error(response.statusText);
+			}
+
+			return response.json() as Promise<LyneAPI.Rankings>;
+		})
+		.then((data) => {
+			for (const item of data.items) {
+				players.push({
+					username: item.user.username,
+					totalPp: item.totalPp
+				});
+			}
+		});
+
+	// Get players
+	for (const player of players) {
+		// eslint-disable-next-line no-await-in-loop
+		const user = await fetch("https://api.zxz.moe/users/" + player.username + "/scores").then(
+			async (response) => {
+				if (!response.ok) {
+					throw new Error(`Error when fetching user ${player.username}: ${response.statusText}`);
+				}
+
+				return response.json() as Promise<LyneAPI.UserScores>;
+			}
+		);
+
+		// Total PP less than lowest score, we can quit now.
+		if (player.totalPp < lowest) {
+			break;
+		}
+
+		const topScores = user.items;
+
+		for (const score of topScores) {
+			if (score.pp > lowest || plays.length < leaderboardLength) {
+				score.user = player.username;
+				plays.push(score);
+			} else {
+				break;
+			}
+		}
+
+		plays.sort((a, b) => b.pp - a.pp);
+		plays = plays.slice(0, leaderboardLength);
+		lowest = plays.at(-1)!.pp ?? -2;
+		console.log(`Processed ${player.username}, lowest is now ${lowest}`);
+	}
 
 	setHeaders({
 		"cache-control": "max-age=7200, public"
 	});
 
-	return data;
+	return { topPlays: plays, date: new Date() };
 };
